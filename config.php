5540d680f97f0db274c4f2c653187c11d5470a12
Elvin Jay <elvinjay.kinatac-an@msubuug.edu.ph>
2026-05-22 15:20:41 +0800

    Update config.php

--- config.php
+++ config.php
@@ -6,10 +6,16 @@ define('DB_USER', getenv('MYSQLUSER') ?: 'root');
 define('DB_PASS', getenv('MYSQLPASSWORD') ?: 'cczjDiTfrcCCKmRDQUhhMhpmmUyZPfzv');
 
 function getDB() {
-    $dsn = "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=utf8mb4";
     try {
-        $pdo = new PDO($dsn, DB_USER, DB_PASS);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo = new PDO(
+            "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=utf8mb4",
+            DB_USER,
+            DB_PASS,
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]
+        );
         return $pdo;
     } catch (PDOException $e) {
         die(json_encode(['error' => 'DB failed: ' . $e->getMessage()]));
